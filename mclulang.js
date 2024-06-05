b331112de8e84221d8a590d908f02234f244c2ed
Mariano Guerra <mariano@marianoguerra.org>
2024-06-05 19:11:40 +0200

    sort code in similar orden everywhere

--- mclulang.js
+++ mclulang.js
@@ -76,49 +76,60 @@ export class Env {
   }
 }
 export const NIL_TAG = mkTag("Nil", Nil),
-  INT_TAG = mkTag("Int", BigInt),
-  FLOAT_TAG = mkTag("Float", Number),
-  STR_TAG = mkTag("Str", String),
   PAIR_TAG = mkTag("Pair", Pair),
   NAME_TAG = mkTag("Name", Name),
   BLOCK_TAG = mkTag("Block", Block),
-  ARRAY_TAG = mkTag("Array", Array),
-  MAP_TAG = mkTag("Map", Map),
   MSG_TAG = mkTag("Msg", Msg),
   SEND_TAG = mkTag("Send", Send),
   LATER_TAG = mkTag("Later", Later),
+  INT_TAG = mkTag("Int", BigInt),
+  FLOAT_TAG = mkTag("Float", Number),
+  STR_TAG = mkTag("Str", String),
+  ARRAY_TAG = mkTag("Array", Array),
+  MAP_TAG = mkTag("Map", Map),
   TAG_TAG = mkTag("Tag", Symbol);
 import * as ohm from "ohm-js";
 export const grammar = ohm.grammar(`McLulang {
     Main = Send
-    Send = Value Msg*
+    nil = "(" ")"
+    Pair = PairHead ":" Value
+    PairHead = Block | Array | Map | Scalar | Later | ParSend
+    name = (letter | "_") (letter | "_" | digit)*
+    Block = "{" Exprs "}"
+    Exprs = Send ("," Send )*
     Msg = verb Value
+    MsgQuote = "\\\\" Msg
     verb = verbStart verbPart*
     verbStart = "+" | "-" | "*" | "/" | "-" | "%" | "&" | "<" | ">" | "!" | "?" | "." | letter
     verbPart = verbStart | digit
-    Value = Pair | PairHead
-    Pair = PairHead ":" Value
-    PairHead = Block | Array | Map | Scalar | Later | ParSend
-    Block = "{" Exprs "}"
-    Array = "[" "]" -- empty
-      | "[" Exprs "]" -- items
-    Exprs = Send ("," Send )*
-    Map = "#" "{" "}" -- empty
-      | "#" "{" Pairs "}" -- items
-    Pairs = Pair ("," Pair)*
-    Scalar = float | int | str | nil | name | MsgQuote
-    Later = "@" Value
+    Send = Value Msg*
     ParSend = "(" Send ")"
+    Later = "@" Value
+    Value = Pair | PairHead
+    Scalar = float | int | str | nil | name | MsgQuote
     float = digit+ "." digit+
     int = digit+
     str = "'" (~"'" any)* "'"
-    nil = "(" ")"
-    name = (letter | "_") (letter | "_" | digit)*
-    MsgQuote = "\\\\" Msg
+    Array = "[" "]" -- empty
+      | "[" Exprs "]" -- items
+    Map = "#" "{" "}" -- empty
+      | "#" "{" Pairs "}" -- items
+    Pairs = Pair ("," Pair)*
   }`),
   semantics = grammar.createSemantics().addOperation("toAst", {
-    Later: (_, v) => new Later(v.toAst()),
-    ParSend: (_o, v, _c) => v.toAst(),
+    nil: (_o, _c) => NIL,
+    Pair: (a, _, b) => new Pair(a.toAst(), b.toAst()),
+    name(_1, _2) {
+      return new Name(this.sourceString);
+    },
+    Block: (_o, exprs, _c) => new Block(exprs.toAst()),
+    Exprs: (first, _, rest) =>
+      [first.toAst()].concat(rest.children.map((v) => v.toAst())),
+    Msg: (verb, obj) => new Msg(verb.toAst(), obj.toAst()),
+    verb(_1, _2) {
+      return this.sourceString;
+    },
+    MsgQuote: (_, msg) => msg.toAst(),
     Send(v, msgs) {
       let r = v.toAst();
       for (const msg of msgs.children) {
@@ -126,33 +137,22 @@ export const grammar = ohm.grammar(`McLulang {
       }
       return r;
     },
-    Msg: (verb, obj) => new Msg(verb.toAst(), obj.toAst()),
-    MsgQuote: (_, msg) => msg.toAst(),
-    Pair: (a, _, b) => new Pair(a.toAst(), b.toAst()),
-    Block: (_o, exprs, _c) => new Block(exprs.toAst()),
+    ParSend: (_o, v, _c) => v.toAst(),
+    Later: (_, v) => new Later(v.toAst()),
+    int(_) {
+      return BigInt(this.sourceString);
+    },
+    float(_a, _d, _b) {
+      return parseFloat(this.sourceString);
+    },
+    str: (_1, s, _3) => s.sourceString,
     Array_items: (_o, exprs, _c) => exprs.toAst(),
     Array_empty: (_o, _c) => [],
-    Exprs: (first, _, rest) =>
-      [first.toAst()].concat(rest.children.map((v) => v.toAst())),
     Map_items: (_h, _o, exprs, _c) =>
       new Map(exprs.toAst().map((p, _i, _) => [p.a, p.b])),
     Map_empty: (_h, _o, _c) => new Map(),
     Pairs: (first, _, rest) =>
       [first.toAst()].concat(rest.children.map((v) => v.toAst())),
-    float(_a, _d, _b) {
-      return parseFloat(this.sourceString);
-    },
-    int(_) {
-      return BigInt(this.sourceString);
-    },
-    str: (_1, s, _3) => s.sourceString,
-    name(_1, _2) {
-      return new Name(this.sourceString);
-    },
-    verb(_1, _2) {
-      return this.sourceString;
-    },
-    nil: (_o, _c) => NIL,
   }),
   parse = (code) => {
     const matchResult = grammar.match(code);
